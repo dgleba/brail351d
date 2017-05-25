67a5166126c14a49bb98db05286b923e3fbf173d
albe <albe@pmdsdata9.local>
2017-05-25 12:36:20 -0400

    paper_trail - 3. setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_paper_trail_whodunnit
+  
+
   protect_from_forgery with: :null_session
   
   #  protect_from_forgery with: :exception
